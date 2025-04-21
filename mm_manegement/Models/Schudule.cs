4e88027391310d3f6cd525f6419ee26a61a5101f
DirectKidman <trumpet.programing525@gmail.com>
2025-04-21 19:51:09 +0900

    スケジュールモデルの追加

---
+++ mm_manegement/Models/Schudule.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mm_manegement.Models
+{
+    public class Schudule
+    {
+        public int Id { get; set; }
+        public required string UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public required IdentityUser User { get; set; }
+        public required string Title { get; set; }
+        public string? Description { get; set; }
+        public DataType StartTime { get; set; }
+        public DataType EndTime { get; set; }
+        public DataType CreatedAt { get; set; }
+        public DataType UpdatedAt { get; set; }
+    }
+}
