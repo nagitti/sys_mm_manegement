be72cecc4dea4eaf0bfb806a5ae6a3eb3393b50f
DirectKidman <trumpet.programing525@gmail.com>
2025-04-21 21:53:49 +0900

    グループモデルの作成。一応グループ作成者とメンバーで分けられるように作ってあります。

---
+++ mm_manegement/Models/Group.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace mm_manegement.Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<GroupMembership> GroupMemberships { get; set; }
+    }
+}
---
+++ mm_manegement/Models/GroupMembership.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mm_manegement.Models
+{
+    public class GroupMembership
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public virtual IdentityUser User { get; set; }
+
+        public int GroupId { get; set; }
+        [ForeignKey(nameof(GroupId))]
+        public virtual Group Group { get; set; }
+
+        public GroupRole role { get; set; }
+    }
+
+    public enum GroupRole
+    {
+        Admin,
+        Member,
+    }
+}
