d17670c3738980b9713f895b67269f902fac6dba
DirectKidman <trumpet.programing525@gmail.com>
2025-04-21 21:40:01 +0900

    簡易的なタグを追加

--- mm_manegement/Models/Schudule.cs
+++ mm_manegement/Models/Schudule.cs
@@ -12,9 +12,16 @@ namespace mm_manegement.Models
         public required IdentityUser User { get; set; }
         public required string Title { get; set; }
         public string? Description { get; set; }
+        public ScheduleTag tag { get; set; }
         public DataType StartTime { get; set; }
         public DataType EndTime { get; set; }
         public DataType CreatedAt { get; set; }
         public DataType UpdatedAt { get; set; }
+    }
+    public enum ScheduleTag
+    {
+        Trpg,
+        MurderMystery,
+        Private,
     }
 }
